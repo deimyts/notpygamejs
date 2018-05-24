25d8908edd0fcda674aec37f8dd61137e6f2611a
Cameron Jones <deimyts@gmail.com>
2018-05-24 01:15:23 -0500

    test(agent): test default properties

--- test/app.spec.js
+++ test/app.spec.js
@@ -12,14 +12,14 @@ describe('when creating an Agent', () => {
     const agent = new Agent;
 
     expect(agent.pos).to.be.an('object');
-    expect(agent.dir)
-    expect(agent.s1)
-    expect(agent.s2)
-    expect(agent.brain)
-    expect(agent.speed)
-    expect(agent.boost)
-    expect(agent.health)
-    expect(agent.rep)
-    expect(agent.selected)
+    expect(agent.dir).to.be.a('number');
+    expect(agent.s1).to.equal(0);;
+    expect(agent.s2).to.equal(0);
+    expect(agent.brain).to.be.an('object');
+    expect(agent.speed).to.equal(4);
+    expect(agent.boost).to.equal(0);
+    expect(agent.health).to.equal(1);
+    expect(agent.rep).to.equal(0);
+    expect(agent.selected).to.be.false;
   })
 })
