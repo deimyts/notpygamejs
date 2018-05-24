70783a47ad4b8a2a236e2e210c3d8fe6f4786ce3
Cameron Jones <deimyts@gmail.com>
2018-05-24 01:17:15 -0500

    refactor(tests): remove placeholder

--- test/app.spec.js
+++ test/app.spec.js
@@ -3,10 +3,6 @@ const expect = require('chai').expect;
 const app = require('../src/index.js')
 const Agent = app.Agent;
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(2+2).to.equal(4);
-});
-
 describe('when creating an Agent', () => {
   test('it should set the default properties', () => {
     const agent = new Agent;
