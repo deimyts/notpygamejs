f42372a4b49b45006662910cb2df4fe96e51dbe8
Cameron Jones <deimyts@gmail.com>
2018-05-29 11:22:50 -0500

    refactor(tests): remove duplicate code

--- test/brain.spec.js
+++ test/brain.spec.js
@@ -3,15 +3,18 @@ const expect = require('chai').expect;
 const Brain = require('../src/Brain.js')
 
 describe('an agent\'s brain', () => {
+  let brain;
+  
+  beforeEach(() => {
+    brain = new Brain();
+  })
+
   test('it should exist', () => {
-    const brain = new Brain();
     expect(brain).to.not.be.undefined;
     expect(brain).to.be.an('object');
   })
 
   test('it should set the default properties', () => {
-    const brain = new Brain();
-
     expect(brain).to.have.property('size');
     expect(brain).to.have.property('density');
     expect(brain).to.have.property('mutationRate');
