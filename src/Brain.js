80da57988a396c5b9c72c4b071adfea2efe223d0
Cameron Jones <deimyts@gmail.com>
2018-05-31 18:47:32 -0500

    fix(brain): correct ref to "ix" property

--- src/Brain.js
+++ src/Brain.js
@@ -61,7 +61,7 @@ Brain.prototype = {
             if(randf(0,1)<this.mutationRate) m += randn(0,this.mutationSeverity);
             this.weights[i][j]= m;
             
-            m= parentBrain.ix[i][j];
+            m= parentBrain.index[i][j];
             if(randf(0,1)<this.mutationRate) m = randi(0,this.size);
             this.index[i][j]= m;
           }
