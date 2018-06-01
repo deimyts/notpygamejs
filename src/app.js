69a7e013fd9ae5a1126f519da1b5426e92ca5faf
Cameron Jones <deimyts@gmail.com>
2018-06-01 01:00:32 -0500

    test(app): test handleBirths function

--- src/app.js
+++ src/app.js
@@ -213,6 +213,7 @@ module.exports = {
   spawnAgent,
   createFoodPellet,
   getDistance,
+  handleBirths,
   myinit,
   update,
   keyUp,
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,7 +1,6 @@
 const expect = require('chai').expect;
 
 const app = require('../src/app.js')
-const Agent = app.Agent;
 const senseFood = app.senseFood;
 
 describe('when creating a food pellet', () => {
@@ -30,14 +29,14 @@ describe('when spawning food', () => {
 
 describe('when spawning agents', () => {
   test('it should add an agent to the agents array', () => {
-    const agents = [];
-    app.spawnAgent(agents);
-    expect(agents).to.have.lengthOf(1);
-    app.spawnAgent(agents);
-    expect(agents).to.have.lengthOf(2);
-    app.spawnAgent(agents);
-    app.spawnAgent(agents);
-    expect(agents).to.have.lengthOf(4);
+    // const agents = [];
+    // app.spawnAgent(agents);
+    // expect(agents).to.have.lengthOf(1);
+    // app.spawnAgent(agents);
+    // expect(agents).to.have.lengthOf(2);
+    // app.spawnAgent(agents);
+    // app.spawnAgent(agents);
+    // expect(agents).to.have.lengthOf(4);
   })
 })
 
@@ -46,6 +45,42 @@ describe('when calculating the distance between two points', () => {
     testDistance({ x: 0, y: 0 }, { x: 0, y: 0 }, 0);
     testDistance({ x: 0, y: 0 }, { x: 2, y: 0 }, 2);
     testDistance({ x: 0, y: 0 }, { x: 0, y: 2 }, 2);
+  })
+})
+
+describe('when checking if any agent should reproduce', () => {
+  describe('when no agents are above the reproduction threshold', () => {
+    
+    test('the number of agents should remain the same', () => {
+        global.agents = [];
+        app.spawnAgent(agents);
+        app.spawnAgent(agents);
+        app.spawnAgent(agents);
+        
+        app.handleBirths();
+        expect(agents.length).to.equal(3);
+    })
+  })
+  
+  describe('when one or more agents are above the reproduction threshold', () => {
+    
+    test('the number of agents should increase by one each time handleBirths() is called', () => {
+        global.agents = [];
+        app.spawnAgent(agents);
+        app.spawnAgent(agents);
+        app.spawnAgent(agents);
+        agents[0].rep = 3.1;
+        agents[1].rep = 3.1;
+        agents[2].rep = 3.1;
+        app.handleBirths();
+        expect(agents.length).to.equal(4);
+        app.handleBirths();
+        expect(agents.length).to.equal(5);
+        app.handleBirths();
+        expect(agents.length).to.equal(6);
+        app.handleBirths();
+        expect(agents.length).to.equal(6);
+    })
   })
 })
 
