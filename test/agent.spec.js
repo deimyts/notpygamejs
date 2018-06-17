38d3363ae8b94e7c88f42d54c5da8029b05c546d
Cameron Jones <deimyts@gmail.com>
2018-06-16 22:18:19 -0500

    refactor(agent): clean up agent property tests

--- test/agent.spec.js
+++ test/agent.spec.js
@@ -8,17 +8,17 @@ describe('when creating an Agent', () => {
   test('it should set the default properties', () => {
     const agent = new Agent;
 
-    expect(agent.pos).to.be.an('object');
-    expect(agent.dir).to.be.a('number');
-    expect(agent.s1).to.equal(0);;
-    expect(agent.s2).to.equal(0);
-    expect(agent.brain).to.be.an('object');
-    expect(agent.speed).to.equal(4);
-    expect(agent.boost).to.equal(0);
-    expect(agent.health).to.equal(1);
-    expect(agent.rep).to.equal(0);
-    expect(agent.foodloss).to.equal(0.002);
-    expect(agent.selected).to.be.false;
+    expect(agent).to.have.property('pos');
+    expect(agent).to.have.property('dir');
+    expect(agent).to.have.property('s1');
+    expect(agent).to.have.property('s2');
+    expect(agent).to.have.property('brain');
+    expect(agent).to.have.property('speed');
+    expect(agent).to.have.property('boost');
+    expect(agent).to.have.property('health');
+    expect(agent).to.have.property('rep');
+    expect(agent).to.have.property('foodloss');
+    expect(agent).to.have.property('selected');
   })
 })
 
